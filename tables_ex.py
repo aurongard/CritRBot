8812c1148caf9dad051eafad7246cb577b3a996e
Tom Williams <104518122+aurongard@users.noreply.github.com>
2023-06-12 21:26:31 -0400

    Create tables_ex.py

---
+++ tables_ex.py
@@ -0,0 +1,19 @@
+magic_fails = ('1. Spell Burn. \n2-10. Spell Backfire. \n11-19. Magic Drain. \n20. Nothing Happens.')
+
+magic_h_fails = ("1. Spell Burn. \n2. Uncontrolled Casting. \n3. Uncontrolled Casting. \n4. Wild Magic. \n5-10. Spell Backfire. \n11-'14. Concentration Break. \n15-19. Magic Drain. \n20. Nothing Happens.")
+
+melee_fails = ('1. You hit yourself. \n2-9. Disarmed. \n10-16. Stumble. \n17-19. Embarrassed. \n20. Nothing Happens.')
+
+melee_h_fails = ("1-2. Weapon Break. \n3-4. Throw Weapon. \n5-6. Enemy Advantage. \n7. Stuck Weapon. \n8. Wrong Target. \n9. Hit yourself. \n10-14. Fall prone. \n15. Pulled Muscle (Arms). \n16. Pulled Muscle (Legs). \n17-18. Loss of Nerve. \n19. Broken Item. \n20. A Little Accident.")
+
+nat_fails = ("1. You hit yourself. \n2-6. Stumble. \n7-9. Off Balance. \n10. Nothing Happens.")
+
+nat_h_fails = ("1-2. Ouch! \n3-5. Wild Swing. \n6-7. Slip Up. \n8-10. Loss of Nerve.")
+
+range_fails = ("1. String Snap. \n2-10. Recoil! \n11-16. Dropped Ammo. \n17-19. Embarrassed. \n20. Nothing Happens.")
+
+range_h_fails = ("1-2. Weapon Break. \n3-5. String Break. \n6-8. Loose String. \n9-16. Wrong Target. \n17-18. Ammo Accident. \n19. Pulled Muscle (Upper Body).\n20. Slip Up.")
+
+throw_fails = ("1. Worst throw ever. \n2-5. Bad throw. \n6-9. Over extended! \n10. Horrible throw.")
+
+thrown_h_fails = ("1. Weapon Break. \n2. Pulled Muscle (Arms). \n3-4. Slip Up. \n5-9. Wrong Target. \n10. WTF?")
