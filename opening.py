54b2165a90029fd605ab7bbced4d304e0093782e
Tom Williams <104518122+aurongard@users.noreply.github.com>
2023-06-12 21:25:56 -0400

    Create opening.py

---
+++ opening.py
@@ -0,0 +1,13 @@
+open_statement = ("Your Fate fate has been decided. You rolled a", 
+"The Abyss awaits, you rolled a",
+"The Nine Hells await, you rolled a", 
+"Wow... That's just bad... you rolled a", 
+"Remember, this hurts you way more than it hurts me. You rolled a",
+"You should probably take a long rest. You rolled a",
+"BWAHAHAHAHAHAHA! *cough cough* You rolled a", 
+"You should put your d20 in dice jail. You rolled a",
+"It could be worse, you rolled a",
+"Someone call a Cleric! You rolled a",
+"Who cast bane!? You rolled a",
+"Hopefully you've paid a Bard to sing your songs after you've died. You rolled a",
+"That's so bad, I need to make a Con save... You Rolled a")
